5df8f205a6da37d8956c5ffa3937262a7e2dc76a
odogwukelly <odogwukellyice@gmail.com>
2026-02-03 15:25:00 +0100

    add README.md and requirement.txt

--- app/routes/home.py
+++ app/routes/home.py
@@ -5,3 +5,19 @@ from flask import render_template
 @app.route("/")
 def landing_page():
     return render_template("index.html")
+
+@app.route("/how-it-works")
+def how_it_works_page():
+    return render_template("index.html")
+
+@app.route("/builders")
+def builders_page():
+    return render_template("index.html")
+
+@app.route("/artisans")
+def artisans_page():
+    return render_template("index.html")
+
+@app.route("/contact")
+def contact_page():
+    return render_template("index.html")
