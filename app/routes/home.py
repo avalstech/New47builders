4e6df44d514112bed0205a15f1a92256b1e7e819
odogwukelly <odogwukellyice@gmail.com>
2026-02-04 09:15:54 +0100

    full project restructure

--- app/routes/home.py
+++ app/routes/home.py
@@ -8,16 +8,20 @@ def landing_page():
 
 @app.route("/how-it-works")
 def how_it_works_page():
-    return render_template("index.html")
+    return render_template("how-it-work.html")
 
 @app.route("/builders")
 def builders_page():
-    return render_template("index.html")
+    return render_template("builders.html")
 
 @app.route("/artisans")
 def artisans_page():
-    return render_template("index.html")
+    return render_template("artisans.html")
 
 @app.route("/contact")
 def contact_page():
-    return render_template("index.html")
+    return render_template("contact.html")
+
+@app.route("/start-project")
+def start_project_page():
+    return render_template("start-project.html")
